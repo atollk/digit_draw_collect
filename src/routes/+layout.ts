215a06fb63ae4544127930b4af39039e6fe8f993
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2025-04-12 16:04:02 +0200

    switch to Vue

--- src/routes/+layout.ts
+++
@@ -1,9 +0,0 @@
-export const ssr = false;
-export const prerender = true;
-
-// Handle the base path for client-side routing
-export const load = ({ url }) => {
-	return {
-		url: url.pathname
-	};
-};
